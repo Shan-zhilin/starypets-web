5802939c26ce86a53bcf73f6c6c4b6fa4c94d32b
单志林 <940259702@qq.com>
2022-10-04 20:49:23 +0800

    faet: 引入vscode配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,45 @@
 module.exports = {
   env: {
-      browser: true,
-      es2021: true,
+    browser: true,
+    es2021: true,
+  },
+  globals: {
+    API: 'readonly',
+  },
+  plugins: ['simple-import-sort'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    globals: {
-      API: 'readonly',
-    },
-    plugins: ['simple-import-sort'],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'simple-import-sort/imports': 'error',
+  },
+  overrides: [
+    {
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+      rules: {
+        'simple-import-sort/imports': [
+          'error',
+          {
+            groups: [
+              // react等包放在首位.
+              ['^react', '^@?\\w'],
+              // 绝对路径导入.
+              ['^(@|components)(/.*|$)'],
+              // 其他文件夹下的文件
+              ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+              // 当前同级文件下的文件.
+              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$', '^\\u0000'],
+              // 样式.
+              ['^.+\\.?(css)$', '^.+\\.?(scss)$', '^.+\\.?(less)$'],
+            ],
+          },
+        ],
       },
-      ecmaVersion: 'latest',
-      sourceType: 'module',
     },
-    rules: {
-      'simple-import-sort/imports': 'error',
-      'simple-import-sort/exports': 'error',
-    },
-    overrides: [
-      // override "simple-import-sort" config
-      {
-        files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
-        rules: {
-          'simple-import-sort/imports': [
-            'error',
-            {
-              groups: [
-                // react等包放在首位.
-                ['^react', '^@?\\w'],
-                // 绝对路径导入.
-                ['^(@|components)(/.*|$)'],
-                // 其他文件夹下的文件
-                ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
-                // 当前同级文件下的文件.
-                ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
-                // 其他的导入.
-                ['^\\u0000'],
-                // 样式.
-                ['^.+\\.?(css)$', '^.+\\.?(scss)$', '^.+\\.?(less)$'],
-              ],
-            },
-          ],
-        },
-      },
-    ],
-  };
+  ],
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {UserConfig } from 'vite';
+import ViteEslint from 'vite-plugin-eslint'
 import { AntdResolve,createStyleImportPlugin } from 'vite-plugin-style-import';
+import StyleLintPlugin from 'vite-plugin-stylelint'
 
 export default (): UserConfig => {
   return {
     base: './',
     plugins: [
       react(),
+      ViteEslint(),
+      StyleLintPlugin({fix:true}),
       createStyleImportPlugin({ resolves: [AntdResolve()] }),
     ],
     resolve: {
