9a82965fccea3e893ef8f8b324e35abf484f553b
shanzhilin <shanzhilin@fenxianglife.com>
2022-11-07 09:28:57 +0800

    fix: 修改详情页

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -2,11 +2,11 @@
  * @Author: shanzhilin
  * @Date: 2022-10-03 20:35:03
  * @LastEditors: shanzhilin
- * @LastEditTime: 2022-10-03 20:40:15
+ * @LastEditTime: 2022-11-07 09:27:47
  */
 
 import React from 'react';
 
-const About: React.FC = () => <div> About </div>;
+const About: React.FC = () => <div> About 没有更新 </div>;
 
 export default About;
