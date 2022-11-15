009df635e58b90fb0a765763da733ddf81d6930b
单志林 <940259702@qq.com>
2022-11-15 23:05:57 +0800

    feat: 增加管理员列表

--- src/pages/AdminList/index.tsx
+++ src/pages/AdminList/index.tsx
@@ -2,12 +2,18 @@
  * @Author: shanzhilin
  * @Date: 2022-11-06 14:44:26
  * @LastEditors: shanzhilin
- * @LastEditTime: 2022-11-07 22:41:02
+ * @LastEditTime: 2022-11-15 22:20:06
  */
 import React from 'react';
+import { Table } from 'antd';
 
 const AdminList: React.FC = () => {
-  return <div>管理员列表</div>;
+  return (
+    <div className="p-20">
+      <div className="rounded-[20px] bg-white p-20"></div>
+      <div className="m rounded-[20px] bg-white p-20 mt-20"></div>
+    </div>
+  );
 };
 
 export default AdminList;
